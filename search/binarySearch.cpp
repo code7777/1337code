daed4d3cc20c437066e9235c4b11f0568f93bdbd
Preston Porter <prestonp2018@gmail.com>
2025-05-05 00:30:02 -0500

    WIP BINSEARCH

---
+++ search/binarySearch.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    public:
+        int search(vector<int>& nums, int target) {
+    
+            bool isOdd = false;
+            int medien = 0;
+            int len = nums.size();
+            if(( len %2 ) != 0){
+                isOdd = true;
+                std:: cout << "isOdd is " << isOdd << std::endl;
+            }
+            if(isOdd){
+                medien = ((size -1)/2)+1;
+                /* if target > medien
+                then see if target is 3/4 (in between medien and end)
+                if target < medien 
+                then see if target is 1/4 in between medien and end  
+                repeat midpoint of nodes until u find target
+                */  
+                 
+            }
+            else {
+                medien = size/2;
+            }
+            
+        }
+    };
