d0bd4403115377c3a9657975b8d6f23df934e3b9
Preston Porter <prestonp2018@gmail.com>
2025-06-07 13:20:14 -0500

    added this to avoid overshadowing  232 Implement Queue using stacks

--- Queue/232.ImplementQueueUsingStacks.cs
+++ Queue/232.ImplementQueueUsingStacks.cs
@@ -1,25 +1,93 @@
-using System.Collections.Generic; //for stacks
+/*
+232. Implement Queue using Stacks
 
-public class MyQueue {
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
 
-    public MyQueue() {
-        
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+ 
+
+Example 1:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+ 
+
+Constraints:
+
+1 <= x <= 9
+At most 100 calls will be made to push, pop, peek, and empty.
+All the calls to pop and peek are valid. */
+
+
+// using System.Collections.Generic; //for stacks
+
+/* normally stacks are LIFO (last in , first out)
+   queues are FIFO (First in,  first out) */
+public class MyQueue
+{
+
+    Stack<int> enqueStack = null; //for enqueing
+    Stack<int> dequeStack = null; //for dequeing
+
+    public MyQueue()
+    {
+
+        this.enqueStack = new Stack<int>();
+        this.dequeStack = new Stack<int>();
     }
-    
-    public void Push(int x) {
-        
+
+    public void Push(int x)
+    {
+        enqueStack.Push(x);
     }
-    
-    public int Pop() {
-        
+
+    public int Pop()
+    {
+        ShiftStacks();
+        return dequeStack.Pop();
     }
-    
-    public int Peek() {
-        
+
+    public int Peek()
+    {
+        ShiftStacks();
+        return dequeStack.Peek();
+
     }
-    
-    public bool Empty() {
-        
+
+    public bool Empty()
+    {
+        return (enqueStack.Count == 0) && (dequeStack.Count == 0);
+    }
+
+    public void ShiftStacks()
+    {
+        if (dequeStack.Count == 0)
+        {
+            while (enqueStack.Count > 0)
+            {
+                dequeStack.Push(enqueStack.Pop());
+            }
+        }
     }
 }
 
