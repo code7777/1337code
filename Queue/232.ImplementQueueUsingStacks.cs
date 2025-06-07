ebfc31aa25c7a4301c1dfed0d12bfec8569d361d
Preston Porter <prestonp2018@gmail.com>
2025-06-07 13:17:01 -0500

    232 Queue null Reference

---
+++ Queue/232.ImplementQueueUsingStacks.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic; //for stacks
+
+public class MyQueue {
+
+    public MyQueue() {
+        
+    }
+    
+    public void Push(int x) {
+        
+    }
+    
+    public int Pop() {
+        
+    }
+    
+    public int Peek() {
+        
+    }
+    
+    public bool Empty() {
+        
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Peek();
+ * bool param_4 = obj.Empty();
+ */
